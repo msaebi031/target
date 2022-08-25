10db9b9b3a3b719600c6769ddbc1ac2022d3524b
ali-akbar <aliakbar.1390.es@gmail.com>
2022-08-25 17:31:18 +0430

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@mui/material";
 import theme from "../components/Theme";
 import "../public/css/main.css";
 // aliii 22e
+
+// ok
 function MyApp({ Component, pageProps }) {
   //test23
   return (
