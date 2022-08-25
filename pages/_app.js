f9a4a6aed92a28aadec0c704a730821f2ba9ce0c
msaebi031 <44286111+msaebi031@users.noreply.github.com>
2022-08-25 17:24:06 +0430

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import theme from "../components/Theme";
 import "../public/css/main.css";
 
 function MyApp({ Component, pageProps }) {
-  //test
+  //test2
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps}  />
