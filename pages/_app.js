2e76ea419b6563fcd03ea52134050406c2643ae8
ali-akbar <aliakbar.1390.es@gmail.com>
2022-08-25 18:00:28 +0430

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import { ThemeProvider } from "@mui/material";
 import theme from "../components/Theme";
 import "../public/css/main.css";
-// aliii 22e
 
-// ok55
-//ok2
 function MyApp({ Component, pageProps }) {
-  //test23444
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
