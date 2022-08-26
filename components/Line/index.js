39686c9b24cd7bb38847e3d5036fa37d98632470
ali-akbar <aliakbar.1390.es@gmail.com>
2022-08-26 09:19:32 +0430

    margin

--- components/Line/index.js
+++ components/Line/index.js
@@ -4,16 +4,16 @@ const Line = ({ zIndex }) => {
   return (
     <Box
       display="flex"
-      justifyContent="space-between"
+      justifyContent="space-around"
       zIndex={zIndex}
       className="w-100 h-100 absolute right-0"
     >
       <Divider
         orientation="vertical"
-        sx={{
-          margin: { xs: "0 auto", sm: "0" },
-          marginRight: { sm: "60px" },
-        }}
+        // sx={{
+        //   margin: { xs: "0 auto", sm: "0" },
+        //   marginRight: { sm: "60px" },
+        // }}
         className="witdh-divider"
       />
       <Divider
@@ -30,7 +30,7 @@ const Line = ({ zIndex }) => {
         orientation="vertical"
         className="witdh-divider"
         sx={{
-          marginLeft: { xs: "48px", sm: "60px" },
+          // marginLeft: { xs: "48px", sm: "60px" },
           display: { xs: "none", sm: "block" },
         }}
       />
