c87d817c21a168d1765a17f44bded2e9186a1992
ali-akbar <aliakbar.1390.es@gmail.com>
2022-08-26 18:14:12 +0430

    li

--- components/Work/Nemone.js
+++ components/Work/Nemone.js
@@ -2,31 +2,33 @@ import { Box, Grid, Typography } from "@mui/material";
 
 const Nemone = () => {
   const items = [
-    { src: "/img/work/1.png" },
-    { src: "/img/work/2.png", pt: 10 },
-    { src: "/img/work/3.png" },
-    { src: "/img/work/4.png", pt: 10 },
+    { src: "/img/work/1.png", text: "ربات شلم" },
+    { src: "/img/work/2.png", text: "فروشگاه", pt: 10 },
+    { src: "/img/work/3.png", text: "رزومه شخصی" },
+    { src: "/img/work/4.png", text: "دیجی پرنت", pt: 10 },
   ];
   return (
     <Grid container>
       {items.map((item, index) => (
         <Grid item xs={12} md={6} key={index} p={7}>
-          <Box className="nemone-container" pt={{ xs: 0, md: item.pt }}>
-            <Box className="nemone-middle">
+          <Box pt={{ xs: 0, md: item.pt }}>
+            <Box className="nemone-container">
               <Box
                 component="img"
                 src={item.src}
                 alt="نمونه کار"
                 className="w-100 img-border nemone-image"
               />
-              <Typography
-                component="p"
-                color="light.light"
-                variant="body1"
-                className="font-light nemone-text"
-              >
-                alii
-              </Typography>
+              <Box className="nemone-middle">
+                <Typography
+                  component="p"
+                  color="light.light"
+                  variant="body1"
+                  className="font-light nemone-text"
+                >
+                  {item.text}
+                </Typography>
+              </Box>
             </Box>
           </Box>
         </Grid>
