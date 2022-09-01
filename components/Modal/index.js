6ad52ae8cbe94d5028650ce6269a09e3a7d80f01
ali akbar <aliakbar.1390.es@gmail.com>
2022-09-01 17:13:10 +0430

    modal

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -27,21 +27,22 @@ const Modals = ({ open, handleOpen }) => {
         </Box>
         <Divider className="divider-bg" />
 
-        <Box my={2} textAlign="center">
-          <Typography component="p" variant="body2" color="dark.light" pb={2}>
+          <Typography textAlign="center" mt={2.5} component="p" variant="body2" color="dark.light" pb={2}>
             جهت ثبت سفارش در یکی از سوشال مدیا ها با ما در ارتباط باشید.
           </Typography>
+        <Box mb={3} textAlign="center" display={{xs:"grid" ,sm:"flex"}} justifyContent="center">
           <Button
             className="fixied-1 font-bold"
             href="https://t.me/Target_Designer"
             color="blue"
             variant="contained"
-            sx={{ ml: 2 }}
+            sx={{ ml:{xs:0,sm:2}}}
             target="_blank"
           >
             ارتباط در تلگرام
           </Button>
           <Button
+          sx={{mt:{xs:1.6,sm:0}}}
             className="fixied-2 font-bold"
             href="https://api.whatsapp.com/send?phone=+989909756978"
             color="green"
